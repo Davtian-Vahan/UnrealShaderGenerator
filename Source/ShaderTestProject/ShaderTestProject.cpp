ad382c7c122784513b65f29df7ee31ae85df7e5b
Vahan Davtian <jedavtian@gmail.com>
2023-04-12 15:09:24 +0400

    Cleanup garbage files

--- Source/ShaderTestProject/ShaderTestProject.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ShaderTestProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShaderTestProject, "ShaderTestProject" );
- 
